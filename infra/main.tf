e151ab5e714ef173620725cc4c4f988364ffe04c
DianaLozano <lozadia10@gmail.com>
2026-04-16 20:47:34 -0500

    feat(init): setup static site with terraform and github actions

---
+++ infra/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+   region = "us-east-1"
+ }
+ terraform {
+     backend "s3" {
+     bucket         = "bucket-johansuesucnstate-terraform" # Debes crearlo manualmente una vez
+     key            = "github-actions-demo-diana/terraform.tfstate" # Ruta dentro del bucket
+     region         = "us-east-1"
+     encrypt        = true
+   }
+ }
+
+ resource "aws_s3_bucket" "web" {
+   bucket = "dianalozano-githubaccions-demo"
+ }
+
+ resource "aws_s3_bucket_website_configuration" "web_config" {
+   bucket = aws_s3_bucket.web.id
+   index_document {
+     suffix = "index.html"
+   }
+ }
+ resource "aws_s3_bucket_public_access_block" "web_public" {
+   bucket = aws_s3_bucket.web.id
+   block_public_acls   = false
+   block_public_policy = false
+   ignore_public_acls  = false
+   restrict_public_buckets = false
+ }
+ resource "aws_s3_bucket_policy" "public_read" {
+   bucket = aws_s3_bucket.web.id
+   policy = jsonencode({
+     Version = "2012-10-17",
+     Statement = [{
+       Effect = "Allow",
+       Principal = "*",
+       Action = "s3:GetObject",
+       Resource = "arn:aws:s3:::dianalozano-githubaccions-demo/*"
+     }]
+   })
+ }
+ output "bucket_name" {
+   value = aws_s3_bucket.web.bucket
+ }
+ output "website_url" {
+   value = "http://${aws_s3_bucket.web.bucket}.s3-website-us-east-1.amazonaws.com"
+ }
